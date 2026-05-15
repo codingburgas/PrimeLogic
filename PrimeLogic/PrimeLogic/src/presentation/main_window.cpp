c00c7d66d101ede03d444b849b435fa2b1422efa
MustafaMustafa25 <MHMustafa25@codingburgas.bg>
2026-05-16 00:30:07 +0300

    Add standings, search, statistics and status sections

--- PrimeLogic/PrimeLogic/src/presentation/main_window.cpp
+++ PrimeLogic/PrimeLogic/src/presentation/main_window.cpp
@@ -1,11 +1,31 @@
 #include "presentation/main_window.h"
 
+#include "presentation/matches_panel.h"
+#include "presentation/search_panel.h"
+#include "presentation/statistics_panel.h"
+#include "presentation/standings_panel.h"
+#include "presentation/status_bar.h"
+#include "presentation/teams_panel.h"
+
 #include "raylib.h"
 
 void renderApp(AppState* state)
 {
-    (void)state;
     ClearBackground(Color{ 245, 247, 250, 255 });
-    DrawText("Sports Score Manager", 24, 18, 28, DARKBLUE);
-    DrawText("...", 24, 64, 16, DARKGRAY);
+    DrawText("PrimeLogic - Sports Score Manager", 24, 18, 28, DARKBLUE);
+
+    UiLayout leftLayout = { 24, 64, 430, 30 };
+    UiLayout rightLayout = { 490, 64, 760, 30 };
+
+    renderTeamsSection(state, &leftLayout);
+    addPanelSpace(&leftLayout, 18);
+    renderMatchesSection(state, &leftLayout);
+
+    renderSearchSection(state, &rightLayout);
+    addPanelSpace(&rightLayout, 18);
+    renderStatisticsSection(state, &rightLayout);
+    addPanelSpace(&rightLayout, 18);
+    renderStandingsSection(state, &rightLayout);
+    addPanelSpace(&rightLayout, 18);
+    renderStatusSection(state, &rightLayout);
 }
